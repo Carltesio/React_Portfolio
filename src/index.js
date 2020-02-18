53af049b183d418d99a37cfc8ab2cb53b33f9910
Carlos <carlos.delgado@hotmail.it>
2020-02-18 10:48:21 +0100

    creating About project pages and tests

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import About from "./About";
 import Hello from "./Hello";
+import Header from "./Header";
+import Footer from "./Footer";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 
 const App = () => {
-  return <Hello />;
+  return (
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Hello}></Route>
+        <Route exact path="/about" component={About}></Route>
+        <Route exact path="/projects" component={Projects}></Route>
+      </Switch>
+      <Footer />
+    </>
+  );
 };
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<BrowserRouter>
+                <App />
+                </BrowserRouter>,
+                document.getElementById("app"));
