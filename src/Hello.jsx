b9779007bd896106c2741f18cdc014409ce5ec95
Carlos <carlos.delgado@hotmail.it>
2020-03-11 23:23:15 +0100

    adding first image slider functionalities

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -10,7 +10,8 @@ const Hello = () => {
      
 
 
-<div className="hero-image hero-about" style={{ background: 'url("./src/pic/background.jpg")', backgroundSize: 'cover', height: '95vh'}}>
+<div className="ui large" style={{ 
+  background: 'url("/src/pic/IMG_1696.png")', backgroundSize: 'cover', height: '95vh', }}>
 
 
         
